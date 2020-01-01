61c7469d73daaea01e0182eb3a46b9eb8518e594
Márcio Pessoa <marcio.pessoa@sciemon.com>
2020-01-01 02:33:23 -0300

    Minor updates

--- session.py
+++ session.py
@@ -7,33 +7,7 @@ people:
   developers:
   - name: Marcio Pessoa
     email: marcio.pessoa@gmail.com
-change-log:
-  2019-09-03
-  - version: 0.7
-    fixed: String receive and parsing (only on Python 3).
-  2019-01-08
-  - version: 0.06
-    fixed: Minor updated to support serial on Python 3.
-  2017-07-27
-  - version: 0.05b
-    added: Featured colours to 'ok' and 'nok' received strings.
-  2017-06-04
-  - version: 0.04b
-    added: Added comment parser to '()'
-  2017-06-02
-  - version: 0.03b
-    added: Added comment parser to ';'
-  2017-05-11
-  - version: 0.02b
-    added: Added is_ready() method.
-    fixed: Added new line to send and receive screen output.
-    added: Added command string filter in send() method.
-  2017-02-21
-  - version: 0.01b
-    added: Added information messages.
-  2016-02-19
-  - version: 0.00b
-    added: Scrach version.
+change-log: Check README.md file.
 """
 
 import sys
